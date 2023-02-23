c7dc4c1e70465f74f0dcf033242d8812611ab95e
Joan D <joandidachr@gmail.comm>
2023-02-23 11:09:09 +0100

    Navbar, h1, h3 and p components

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+const Heading = () => {
+
+    return (
+      <div className="headingDiv" >
+      <heading className="heading">
+        <h1>Welcome to React Mongo Lover</h1>  
+      </heading>
+      </div>
+    )
+  }
+
+  export default Heading;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+function Navbar() {
+  return (
+          <nav className="navbar">
+          <ul><a href="#">Home</a></ul>
+          <ul><a href="#">Contact</a></ul>
+          <ul><a href="#">About </a></ul>
+          </nav>
+  )
+
+}
+
+export default Navbar;
